45527cb914bc896c30945c2f60f307e7c282bce5
YVONNE EDER <S1810237012@students.fh-hagenberg.at>
2019-05-21 12:54:11 +0200

    useful messafe 305

--- janoliver02_Novychen_android/dummy/src/TimeToText.java
+++ janoliver02_Novychen_android/dummy/src/TimeToText.java
@@ -2,7 +2,11 @@
 public class TimeToText {
 
 
-	public void thisIsANewMethod() { }
+	public void thisIsANewMethod() {
+		
+	String x = "Help us";
+
+	 }
 
 	public static void main(String[] args) {
 	
